8e74f786b71e5b24b05f407f63375003779dfa10
Pablo Pineda <pablo.pineda@galileo.edu>
2024-05-02 22:49:56 -0600

    Tutorial 1 terminado

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
+import { loader as rootLoader, action as rootAction, } from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
